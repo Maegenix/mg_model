5d3b5657d9676e54f5841294f6b32d42eefd3ab7
dmitrievichlevin <jhowar39@emich.edu>
2023-10-15 21:41:58 -0700

    Initial Commit

---
+++ src/mg_model/token.py
@@ -0,0 +1,23 @@
+import binascii
+import os
+
+from mongoengine import Document, fields, CASCADE
+from django.utils import timezone
+from mg_model.user import User
+
+
+class Token(Document):
+    key = fields.StringField(required=True)
+    user = fields.ReferenceField(User, reverse_delete_rule=CASCADE)
+    created = fields.DateTimeField(default=timezone.now)
+
+    def save(self, *args, **kwargs):
+        if not self.key:
+            self.key = self.generate_key()
+        return super(Token, self).save(*args, **kwargs)
+
+    def generate_key(self):
+        return binascii.hexlify(os.urandom(20)).decode()
+
+    def __str__(self):
+        return self.key
