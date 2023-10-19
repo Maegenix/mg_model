bd5422302c0a08522f8cdcb4242961ca7f5e8c66
Nikolai <85088766+DmitrievichLevin@users.noreply.github.com>
2023-10-19 15:48:10 -0700

    moved serializer into model

--- src/mg_model/token.py
+++ src/mg_model/token.py
@@ -1,10 +1,39 @@
 import binascii
 import os
+import json
+from bson.objectid import ObjectId
 
 from mongoengine import Document, fields, CASCADE
 from django.utils import timezone
 from mg_model.user import User
-from mg_model.serializers import Serializer
+
+
+def serialize(self, doc, **kwargs):
+    unwrapped = {}
+    nested_dict = {}
+    unwrapped["model_type"] = doc.__class__.__name__
+    for k in dict(doc._fields).keys():
+        try:
+            if isinstance(doc[k], (User,)):
+                unwrapped[k] = self.serialize(doc[k])
+            elif isinstance(doc[k], ObjectId):
+                unwrapped[k] = doc[k].__str__()
+            elif isinstance(doc[k], dict):
+                for p, l in doc[k].items():
+                    nested_dict[p] = self.serialize(l)
+                unwrapped[k] = nested_dict
+            else:
+                unwrapped[k] = doc[k]
+            # Add Arbitrary values to model packet data
+            for r, v in kwargs.items():
+                if k == r:
+                    merged = dict()
+                    merged.update(unwrapped[k])
+                    merged.update(v)
+                    unwrapped[k] = merged
+        except Exception as e:
+            print("\n", e)
+    return json.dumps(unwrapped) if not dict else unwrapped
 
 
 class Token(Document):
@@ -12,7 +41,7 @@ class Token(Document):
     key = fields.StringField(required=True)
     user = fields.ReferenceField(User, reverse_delete_rule=CASCADE)
     created = fields.DateTimeField(default=timezone.now)
-    to_json = Serializer(Token, User).serialize
+    to_json = serialize
 
     def save(self, *args, **kwargs):
         if not self.key:
