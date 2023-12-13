c7ddaae0dd7bca7a0b2c5e9fde66885316a05cd9
Nikolai <85088766+DmitrievichLevin@users.noreply.github.com>
2023-12-13 00:20:55 -0800

    Update entity.py

--- src/mg_model/Entities/entity.py
+++ src/mg_model/Entities/entity.py
@@ -5,4 +5,5 @@ import uuid
 class Entity(Document):
     name = StringField(max_length=100)
     id = StringField(primary_key=True, default=str(uuid.uuid4()), editable=False)
+    position = ListField(FloatField(), default=[0.0, 0.0, 0.0])
     meta = {"allow_inheritance": True}
