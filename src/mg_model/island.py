9cba9e64695c8bbf74ef88c3712b28b6737b5bf0
Nikolai <85088766+DmitrievichLevin@users.noreply.github.com>
2023-12-13 00:05:49 -0800

    Update island.py

--- src/mg_model/island.py
+++ src/mg_model/island.py
@@ -6,5 +6,5 @@ class Island(Document):
     default = str(uuid.uuid4())
     entities = MapField(field=StringField(max_length=100))
     id = StringField(primary_key=True, default=default, editable=False)
-    id = StringField(primary_key=True, default=f"Island/{default}", editable=False)
+    reference = StringField(primary_key=True, default=f"Island/{default}", editable=False)
     name = StringField(max_length=100)
